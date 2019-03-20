804a39f3034c4d66d318450689dd63bc448913e1
GeetInd <geetb2080@gmail.com>
2019-03-20 16:47:32 -0700

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecare
+{
+    class Car
+    {
+        private string model;
+        private int price;
+        private string color;
+        private double safetyRating;
+        private List<Feature> features;
+       
+
+        public Car(int _price, string _color, double _safetyRating,string _model)
+        {
+            this.model = _model;
+            this.price = _price;
+            this.color = _color;
+            this.safetyRating = _safetyRating;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public double SafetyRating
+        {
+            get { return safetyRating; }
+            set { safetyRating = value; }
+        }
+
+        public List<Feature> OtherFeatures
+        {
+            get { return features; }
+            set { features = value; }
+        }
+    }
+
+    public class Feature
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MakeCare
+            Car BMW = new Car(10000, "black", 4.5, "M-Series");
+            var features = new List<Feature>
+            {
+                new Feature() { Name="Bluetooth", Value="Sony"},
+                new Feature() { Name="HeatedSeat", Value="Exists"},
+
+            };
+
+            BMW.OtherFeatures = features;
+
+            //CheckFeature
+
+            Feature BMWCheckFeature = BMW.OtherFeatures.Find(x => x.Name.Contains("Bluetooth"));
+            if (BMWCheckFeature.Value == "Sony")
+            {
+                Console.WriteLine("Yes, BMW has Sony Bluetooth option");
+            }
+        }
+        
+    }
+}
